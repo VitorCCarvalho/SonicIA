b972f0ee26733190fef315a24058c81d18d0ad6a
Vitor Costa Carvalho <49074487+VitorCCarvalho@users.noreply.github.com>
2020-05-13 18:51:10 -0300

    implementação de ações aleatórias
    foi feito o array de ações aleatórias para as ações do sonic

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
-# First, import retro. Make sure it's installed.
 import retro
+from random import randint
+import numpy as np
 
-# next start an environment to interact with Sonic
 env = retro.make('SonicTheHedgehog-Genesis', state='GreenHillZone.Act1') 
 
-# reset the env to the first frame so we can start interacting
 env.reset() 
 
-# loop forever 
-while True: 
-# env.step takes a random set of button presses and moves the emulator forward one frame 
-    obs, rew, info, done = env.step(env.action_space.sample()) 
-# env.render shows you in a little screen what's happening.
+while True:
+    act = np.random.randint(2,size=12)
+    arr = act[0:12]
+    obs, rew, info, done = env.step(act)
+    
+
     env.render()
